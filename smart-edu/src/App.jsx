2a9f38c909b544c30cd0000c5e6149b5c61ec8e0
laurenzo0 <uzochukwuchinaemerem@gmail.com>
2026-03-17 01:17:07 +0000

    Merge feature and clean up nested smart-edu folder

--- smart-edu/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./index.css";
-import Login from "./components/pages/Login/Login";
-
-function App() {
-  return <Login />;
-}
-
-export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,30 @@
-<<<<<<< HEAD
+import Navbar from "./components/reusableUi/Navbar/Navbar";
+// import "./App.css";
+import Button from "./components/reusableUi/Button/Button";
+import Login from "./components/pages/Login/Login";
+
 import LandingPage from "./components/pages/LandingPage/LandingPage";
 import PracticePage from "./components/pages/PracticePage/PracticePage.jsx";
-
-
-function App() {
-    return (
-        <>
-            <LandingPage />
-            {/* <PracticePage /> */}
-        </>
-    );
-}
-
-export default App;
-=======
-import Navbar from "./components/reusableUi/Navbar/Navbar";
-
-import "./App.css";
-import Button from "./components/reusableUi/Button/Button";
-
-
-import LandingPage from "./components/pages/LandingPage/LandingPage";
 
 // import NarrowSidebar from "./components/reusableUi/Sidebar/NarrowSidebar"
 import Sidebar from "./components/reusableUI/Sidebar/Sidebar";
 
-
-
 function App() {
   return (
     <>
-      <div className="app-layout">
+      {/* <div className="app-layout">
         <Sidebar />
         {/* <NarrowSidebar /> */}
 
-        <div className="main-section">
+        {/*<div className="main-section">
             <Navbar />
             <LandingPage />
-
+            <PracticePage />
         </div>
-      </div>
-
-
-
+      </div> */}
+      <Login />
     </>
   );
 }
 
 export default App;
->>>>>>> 35de45701ebb84739a65b34d478e7dc8017d224e
